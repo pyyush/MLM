2eb8ae4d9936e09bac2056375efe970dfa4e9605
Piyush Vyas <pie.vyas@gmail.com>
2023-04-06 10:51:13 -0500

    Update mlm_fastapi.py
    - use pydantic's BaseModel for type hints
    - add get methods

--- mlm_fastapi.py
+++ mlm_fastapi.py
@@ -1,29 +1,34 @@
 import uvicorn
 from typing import Dict
 from fastapi import FastAPI
+from pydantic import BaseModel
 from transformers import pipeline
-from dataclasses import dataclass
 
 # pre-load pipelines
 xlm_roberta_base = pipeline('fill-mask', model='xlm-roberta-base')
 xlm_roberta_large = pipeline('fill-mask', model='xlm-roberta-large')
 
-# define type hints
-@dataclass
-class Request:
+class Request(BaseModel):
     text: str
     model: str
     
-@dataclass
-class Response:
+class Response(BaseModel):
     text: str
     model: str
     predictions: Dict
 
 # declaring FastAPI instance
 app = FastAPI()
+
+@app.get("/")
+def status():
+    return {"api_status": "OK"}
+
+@app.get("/models")
+def list_models():
+    return {"models": ["xlm-roberta-base", "xlm-roberta-large"]}
  
-@app.post('/predict')
+@app.post('/predict', response_model=Response)
 def predict(request: Request) -> Response:
     preds = {}
     
